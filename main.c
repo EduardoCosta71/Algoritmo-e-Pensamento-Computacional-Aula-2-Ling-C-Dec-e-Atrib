739afe884f3ec54deaf3d9d43231f13c5f4add7c
Eduardo DEV <duboy42@gmail.com>
2026-08-18 16:11:17 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+
+
+int main () {
+
+    float comprimento, largura, area, perimetro;
+
+    printf("Programa Jardim Retangular\n");
+
+    printf("Digite o comprimento do jardim: ");
+
+    printf("Digite o comprimento do jardim: ");
+    scanf("%f", &comprimento);
+
+    printf("Digite a largura do jardim: ");
+    scanf("%f", &largura);
+
+    perimetro = 2 * (comprimento + largura);
+    area = comprimento * largura;
+
+    printf("Perímetro do Jardim: %.2f\n", perimetro);
+    printf("Área do Jardim: %.2f\n", area);
+
+    return 0;
+}
