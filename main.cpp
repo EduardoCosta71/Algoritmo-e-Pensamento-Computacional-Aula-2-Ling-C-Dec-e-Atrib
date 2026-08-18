6043ab5b6096f4df07c0c942a551d124f9e35355
Eduardo DEV <duboy42@gmail.com>
2026-08-18 10:54:53 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	
+	//Declaraçao das variaveis.
+	int idade = 26;
+	float altura = 1.70;
+	char inicial = 'A';
+	unsigned int id_usuario = 45500;
+	
+	//Exibindo os valores na tela.
+	
+	printf("Idade: %d anos\n", idade);
+	printf("Altura: %.2f metros\n", altura);
+	printf("Inicial do nome: %c\n", inicial);
+	printf("ID do usuario: %u\n", id_usuario);
+	return 0;
+	
+	
+}
